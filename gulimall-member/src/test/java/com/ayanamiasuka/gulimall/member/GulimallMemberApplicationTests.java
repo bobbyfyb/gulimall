9598a2fac0a3369a1346729907593c25521f9e74
AyanamiAsuka <fyb0118@gmail.com>
2021-05-15 23:09:45 +0900

    项目初始结构创建

---
+++ gulimall-member/src/test/java/com/ayanamiasuka/gulimall/member/GulimallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayanamiasuka.gulimall.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ gulimall-order/src/test/java/com/ayanamiasuka/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayanamiasuka.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
