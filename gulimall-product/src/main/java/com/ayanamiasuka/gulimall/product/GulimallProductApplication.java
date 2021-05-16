9c87e5dc46734ea74bf7547c9cb84237695aa1f7
AyanamiAsuka <fyb0118@gmail.com>
2021-05-16 22:31:06 +0900

    import renren-fast backend manage system and renren code generator; auto generate code for each micro-service module

--- gulimall-product/src/main/java/com/ayanamiasuka/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/ayanamiasuka/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,25 @@
 package com.ayanamiasuka.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+* 1. 整合mybatis-plus
+*   1). 导入依赖
+*        <dependency>
+            <groupId>com.baomidou</groupId>
+            <artifactId>mybatis-plus-boot-starter</artifactId>
+            <version>3.4.2</version>
+        </dependency>
+*   2). 配置
+*           a. 配置数据源
+*               1). 导入数据库驱动
+*               2). 在application。yaml里配置数据源
+*           b. 配置mybatis-plus
+*               1). 使用@MapperScan
+*               2). 告诉mybatis-plus sql映射文件位置
+ */
+@MapperScan("com.ayanamiasuka.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
