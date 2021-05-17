84f19203ccf9aa2e52a28599409b26de61eedf4c
AyanamiAsuka <fyb0118@gmail.com>
2021-05-17 17:40:34 +0900

    导入Spring-cloud-alibaba模块， 配置nacos注册中心注册微服务， 设置并测试open-feign远程调用功能

--- gulimall-coupon/src/main/java/com/ayanamiasuka/gulimall/coupon/GulimallCouponApplication.java
+++ gulimall-coupon/src/main/java/com/ayanamiasuka/gulimall/coupon/GulimallCouponApplication.java
@@ -2,6 +2,7 @@ package com.ayanamiasuka.gulimall.coupon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 public class GulimallCouponApplication {
--- gulimall-member/src/main/java/com/ayanamiasuka/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/ayanamiasuka/gulimall/member/GulimallMemberApplication.java
@@ -2,8 +2,18 @@ package com.ayanamiasuka.gulimall.member;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+/*
+* 1.想要远程调用别的服务：
+*   1). 引入open-feign
+*   2). 编写一个接口并注解@FeignClient("<远程调用服务名>")告诉SpringCloud这个接口需要调用远程服务
+*   3). 生命接口的每一个方法都是调用远程服务的哪个请求
+*   4). 开启远程调用功能 入口类标注@EnableDiscoveryClient @EnableFeignClients(basePackages = "<存放远程调用FeignClient接口的报名>")
+ */
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients(basePackages = "com.ayanamiasuka.gulimall.member.feign")
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
